991018ac7ce648d9312e4b2dec892d5d0cd73bd1
İbrahim Köylü <119944742+koyluibrahim@users.noreply.github.com>
2023-04-18 23:40:10 +0300

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown(second_input):
+    while second_input:
+        minutes, seconds = divmod(second_input, 60)
+        time_format = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(time_format, end='\r')
+        time.sleep(1)
+        second_input -= 1
+
+    print("stop")
+
+time_input = int(input("Enter the Time in seconds: "))
+
+countdown(time_input)
